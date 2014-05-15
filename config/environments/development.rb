f62c6432b1188a6f30d6554e76a1014cc33d48e8
Waihon Yew <yewwaihon@gmail.com>
2014-05-15 23:03:31 +0800

    Configure

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,16 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com"
+    port: 587
+    domain: ENV["DOMAIN_NAME"]
+    authentication: "plain"
+    enable_starttls_auto: true
+    user_name: ENV["GMAIL_USERNAME"]
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
