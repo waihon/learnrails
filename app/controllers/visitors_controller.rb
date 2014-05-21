169985760c1313b54400222f337ce432ef330c26
Waihon Yew <yewwaihon@gmail.com>
2014-05-21 08:54:58 +0800

    Update Application Layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,9 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
-		render 'visitors/new'	# Make the hidden code explicit
+		#render 'visitors/new'	# Make the hidden code explicit
+		flash.now[:notice] = 'Welcome!'
+		flash.now[:alert] = 'My birthday is soon.'
 	end
 
 end
