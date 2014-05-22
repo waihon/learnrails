192aa1081f1767e61e48702023b847a942715048
Waihon Yew <yewwaihon@gmail.com>
2014-05-23 07:02:23 +0800

    Front-end Framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,8 @@ class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
 		#render 'visitors/new'	# Make the hidden code explicit
-		flash.now[:notice] = 'Welcome!'
-		flash.now[:alert] = 'My birthday is soon.'
+		#flash.now[:notice] = 'Welcome!'
+		#flash.now[:alert] = 'My birthday is soon.'
 	end
 
 end
