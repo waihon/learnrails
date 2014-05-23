ab6802663acf1db5fbc529419d03a7eb2b2bd040
Waihon Yew <yewwaihon@gmail.com>
2014-05-23 23:12:07 +0800

    Contact Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LearnRails::Application.routes.draw do
-  # root to: redirect('/about.html')
+  #root to: redirect('/about.html')
+  #post 'contact', to: 'contacts#process_form'
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
 end
