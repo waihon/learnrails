258e10244c9c95de57eae336766aceb7adc09c8d
Waihon Yew <yewwaihon@gmail.com>
2014-05-24 15:50:22 +0800

    Mailing list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,10 +1,24 @@
 class VisitorsController < ApplicationController
 
 	def new
-		@owner = Owner.new
-		#render 'visitors/new'	# Make the hidden code explicit
-		#flash.now[:notice] = 'Welcome!'
-		#flash.now[:alert] = 'My birthday is soon.'
+		#@owner = Owner.new
+		@visitor = Visitor.new
 	end
 
+	def create
+		@visitor = Visitor.new(secure_params)
+		if @visitor.valid?
+			@visitor.subscribe
+			flash[:notice] = "Signed up #{@visitor.email}."
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def secure_params
+		params.require(:visitor).permit(:email)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ LearnRails::Application.routes.draw do
   #root to: redirect('/about.html')
   #post 'contact', to: 'contacts#process_form'
   resources :contacts, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
 end
